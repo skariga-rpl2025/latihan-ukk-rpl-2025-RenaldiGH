9a3f49cf349ef610bc8f707c09cd797f136c465a
ReyGH <emelburiq089@gmail.com>
2025-11-24 14:44:29 +0700

    commit

---
+++ LatihanUKK/src/PendaftaranAngotaGYM.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Toshiba
+ */
+import java.util.Scanner;
+
+public class PendaftaranAngotaGYM {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int totalAnggota = 0;
+        String lagi;
+
+        System.out.println("===== PENDAFTARAN ANGGOTA GYM =====\n");
+
+        do {
+            System.out.print("Masukkan nama anggota: ");
+            String nama = input.nextLine();
+            totalAnggota++;
+
+            System.out.print("Daftar anggota lagi? [ya/tidak]> ");
+            lagi = input.nextLine();
+
+            System.out.println(); // biar ada jarak seperti contoh
+
+        } while (lagi.equalsIgnoreCase("ya"));
+
+        System.out.println("----------------------------------------");
+        System.out.println("Total anggota yang didaftarkan: " + totalAnggota);
+
+        input.close();
+    }
+}
